34337053b8145e33e6eaa92299adc16fb4ce0244
SheharaNiwantha <sheharaniwantha1@gmail.com>
2020-09-30 10:31:20 +0530

    Updated

--- app/src/main/java/com/example/intproj/MainActivity4.java
+++ app/src/main/java/com/example/intproj/MainActivity4.java
@@ -38,7 +38,7 @@ public class MainActivity4 extends AppCompatActivity {
 
         Mname = new MenuName();
 
-        butDelete.setOnClickListener(new View.OnClickListener() {
+        /*butDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dbRef = FirebaseDatabase.getInstance().getReference().child("MenuName/Mname1");
@@ -46,7 +46,7 @@ public class MainActivity4 extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Successfull Deleted",Toast.LENGTH_SHORT).show();
 
             }
-        });
+        });*/
 
         butupdate.setOnClickListener(new View.OnClickListener() {
             @Override
