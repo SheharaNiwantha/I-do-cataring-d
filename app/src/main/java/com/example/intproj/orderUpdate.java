cb33e5ca15a63ce445fd137051425ee38f3d4f67
pawanikm <pawanimunasinghe99@gmail.com>
2020-10-03 12:52:04 +0530

    change design.

--- app/src/main/java/com/example/intproj/orderUpdate.java
+++ app/src/main/java/com/example/intproj/orderUpdate.java
@@ -1,5 +1,6 @@
 package com.example.intproj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -98,6 +99,9 @@ public class orderUpdate extends AppCompatActivity {
                                 upRef.setValue(ord);
 
                                 Toast.makeText(getApplicationContext(),"Your changes saved successfully",Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(orderUpdate.this, orders.class);
+                                startActivity(intent);
 
                             }
                             catch (NumberFormatException e){
