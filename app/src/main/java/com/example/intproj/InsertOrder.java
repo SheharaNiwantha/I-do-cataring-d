6907863cfe954061605b1a43e445d41a71d5a45b
Danuri <danuri.alwis@gmail.com>
2020-10-04 16:54:40 +0530

    change design2

--- app/src/main/java/com/example/intproj/InsertOrder.java
+++ app/src/main/java/com/example/intproj/InsertOrder.java
@@ -131,14 +131,14 @@ public class InsertOrder extends AppCompatActivity {
 
                 if (menu == 1){
                 int sum = plates * 1400;
-                tvCal.setText("Total Price = RS: " + String.valueOf(sum));}
+                tvCal.setText("RS: " + String.valueOf(sum));}
                 else if(menu == 2){
                     int sum = plates * 1600;
-                    tvCal.setText("Total Price = RS: " + String.valueOf(sum));
+                    tvCal.setText("RS: " + String.valueOf(sum));
                 }
                 else if (menu == 3) {
                     int sum = plates * 2000;
-                    tvCal.setText("Total Price = RS: " + String.valueOf(sum));
+                    tvCal.setText("RS: " + String.valueOf(sum));
                 }
 
             }
